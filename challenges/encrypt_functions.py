df0b0545d7f1ccd141d38ad4b9ca670d795f3472
LouisVannobel <louisvannob@gmail.com>
2024-07-25 11:19:08 +0200

    add encrypt functions

--- challenges/encrypt_functions.py
+++ challenges/encrypt_functions.py
@@ -1,1 +1,15 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
 
+def rsa_encrypt(public_key_pem, plaintext):
+    public_key = serialization.load_pem_public_key(public_key_pem.encode())
+
+    ciphertext = public_key.encrypt(
+        plaintext.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return ciphertext
